a69a86a1060fbeedab030d768f20a4a310f1d8ae
sulaevatumar67-sys <sulaevatumar67@gmail.com>
2026-02-09 20:00:22 +0600

    server

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,7 +21,7 @@ if not SECRET_KEY:
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,.onrender.com").split(",")
+ALLOWED_HOSTS = ['*']
 
 # ─────────── Приложения ───────────
 INSTALLED_APPS = [
